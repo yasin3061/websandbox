e68343ca85de77ba133a8986be271648c4a292b0
yasinb <yasin.bhojawala@gmail.com>
2016-12-10 12:06:29 +0530

    Project setup for the Spring Social (twitter) integration series
    Project setup for the Spring Social (twitter) integration series

---
+++ spring-twitter-integration-1/src/main/java/org/websandbox/learning/config/WebConfiguration.java
@@ -0,0 +1,5 @@
+package org.websandbox.learning.config;
+
+public class WebConfiguration {
+
+}
---
+++ spring-twitter-integration-1/src/main/java/org/websandbox/learning/home/HomeController.java
@@ -0,0 +1,29 @@
+package org.websandbox.learning.home;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	Twitter twitter;
+
+	@RequestMapping("/")
+	public String hello(@RequestParam(defaultValue = "springmvc") String searchQuery, Model model) {
+		SearchResults searchResults = twitter.searchOperations().search(searchQuery);
+		List<String> tweets = searchResults.getTweets().stream().map(Tweet::getText).collect(Collectors.toList());
+		model.addAttribute("tweets", tweets);
+		return "searchResults";
+	}
+
+}
